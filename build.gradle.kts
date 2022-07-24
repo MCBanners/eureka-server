94ffd87c9987fa52bb03b6a863f87dd0c0726c58
darbyjack <admin@glaremasters.me>
2022-07-23 19:15:13 -0500

    Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+	id("org.springframework.boot") version "2.7.2"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("java")
+}
+
+group = "com.mcbanners"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(17))
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.3"))
+
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+}
+
+tasks {
+	bootJar {
+		archiveFileName.set("eureka.jar")
+	}
+}
